5f0436688f6829ee19eda9a37eeed7ce814c9511
nimeshlathiya <nlathiya40@gmail.com>
2025-01-18 11:40:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
-  
+  const [password, setPassword] = useState("");
 
   const passwordRef = useRef(null);
 
